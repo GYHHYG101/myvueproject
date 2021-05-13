7ac9edd602ad596747ab5e8f10916c0cac9a68f8
gyh <2446125048@qq.com>
2021-05-13 14:46:38 +0800

    Initial commit

---
+++ store/index.js
@@ -0,0 +1,58 @@
+import {createStore} from 'vuex'
+//全局变量文件
+export default createStore({
+    state: {
+        //全局参数
+        tagsList: [],
+        collapse: false
+    },
+    mutations: {
+        //set方法
+        delTagsItem(state, data) {
+            state
+                .tagsList
+                .splice(data.index, 1);
+        },
+        setTagsItem(state, data) {
+            state
+                .tagsList
+                .push(data)
+        },
+        clearTags(state) {
+            state.tagsList = []
+        },
+        closeTagsOther(state, data) {
+            state.tagsList = data;
+        },
+        closeCurrentTag(state, data) {
+            for (let i = 0, len = state.tagsList.length; i < len; i++) {
+                const item = state.tagsList[i];
+                if (item.path === data.$route.fullPath) {
+                    if (i < len - 1) {
+                        data
+                            .$router
+                            .push(state.tagsList[i + 1].path);
+                    } else if (i > 0) {
+                        data
+                            .$router
+                            .push(state.tagsList[i - 1].path);
+                    } else {
+                        data
+                            .$router
+                            .push("/");
+                    }
+                    state
+                        .tagsList
+                        .splice(i, 1);
+                    break;
+                }
+            }
+        },
+        // 侧边栏折叠
+        hadndleCollapse(state, data) {
+            state.collapse = data;
+        }
+    },
+    actions: {},
+    modules: {}
+})
---
+++ views/AboutUs.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <i class="el-icon-lx-redpacket_fill"></i> 公司介绍
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="plugins-tips">简介内容</div>
+<!--            <div>-->
+<!--                <img src="https://lin-xin.gitee.io/images/weixin.jpg" />-->
+<!--            </div>-->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "aboutus"
+};
+</script>
+
+<style>
+</style>
